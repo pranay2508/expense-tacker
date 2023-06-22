c32ae804037003e0c01d3a5364c65faa0baeea52
pranay2508 <bhatnagarpranay8@gmail.com>
2023-06-22 20:29:13 +0530

    .

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -3,41 +3,53 @@ import React, { useState } from "react";
 import "./ExpenseForm.css";
 
 const ExpenseForm = () => {
-  //   const [enteredTitled, setEnteredTitle] = useState("");
-  //   const [enteredAmount, setEnteredAmount] = useState("");
-  //   const [enteredDate, setEnteredDate] = useState("");
+  //below 3 lines are the states updates indivdually
+    const [enteredTitled, setEnteredTitle] = useState("");
+    const [enteredAmount, setEnteredAmount] = useState("");
+    const [enteredDate, setEnteredDate] = useState("");
 
-  // Imp =>> there are two ways to use or manage the stae by object method or by
+  // Imp =>> there are two ways to use or manage the state by object method or by
   // or by creating individual state for each function .
 
-  const [userInput, setUserInput] = useState({
-    enteredTitle: "",
-    enteredAmount: "",
-    enteredDate: "",
-  });
+  // const [userInput, setUserInput] = useState({
+  //   enteredTitle: "",
+  //   enteredAmount: "",
+  //   enteredDate: "",
+  // });
   const titleChangeHandler = (event) => {
-    // setEnteredTitle(event.target.value);
-    setUserInput({
-      ...userInput,
-      enteredTitle: event.target.value,
-    });
+    setEnteredTitle(event.target.value);
+   
+    // setUserInput({
+    //   ...userInput,
+    //   enteredTitle: event.target.value,
+    // });
+
+  // setUserInput((prevState) => {
+  //   return {...prevState , enteredTitle : event.target.value
+  //   };
+  // });
   };
 
   const AmountChangeHandler = (event) => {
-    // setEnteredAmount(event.target.value);
-    setUserInput({
-      ...userInput,
-      enteredAmount: event.target.value,
-    });
+    // this is first method 
+    setEnteredAmount(event.target.value);
+ 
+//  this is 2nd method 
+    //   setUserInput({
+  //     ...userInput,
+  //     enteredAmount: event.target.value,
+  //   });
   };
 
   const DateChangeHandler = (event) => {
-    // setEnteredDate(event.target.value);
-    setUserInput({
-      ...userInput,
-      enteredDate: event.target.value,
-    });
-  };
+    setEnteredDate(event.target.value);
+
+
+  //   setUserInput({
+  //     ...userInput,
+  //     enteredDate: event.target.value,
+  //   });
+   };
 
   return (
     <form>
